b742dba953d550ae38f59edc642832aace0ef25d
Raygud <Runigudmundarson@hotmail.com>
2022-06-15 01:25:09 +0200

    merged

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const mysql = require('mysql');
 var cors = require('cors')
-const importData = require("./data.json")
+const importData = require("./data.json");
+const { send } = require("process");
+const Token = "OnlyRay121294"
 let port = process.env.PORT || 3001;
 require('dotenv').config()
 app.use(cors())
@@ -27,15 +29,24 @@ Connection.connect(function (err) {
     console.log("Connection to Database successful!!" + Connection.database);
 });
 
+
+
 app.get("/", (req, res) => {
     res.send("Hello world")
 })
 
-app.get("/api", (req, res) => {
-    Connection.query("SELECT * FROM Dataset", function (err, result, fields) {
+app.get(`/api`, (req, res) => {
+    Connection.query("SELECT * FROM UsersTest WHERE id='4'", function (err, result, fields) {
         if (err) throw err;
-        console.log(result);
-        res.send(result)
+        exists = result.length
+        console.log(result.length);
+        if (result.length == false) {
+            res.send("Error")
+        }
+        else {
+            res.send(result)
+        }
+
     });
 })
 
@@ -69,9 +80,39 @@ app.post("/api/Post", (req, res) => {
     Connection.query(sql, function (err, result) {
         if (err) throw err;
         console.log("1 record inserted");
-
+        res.send(result)
     });
 })
+
+function UpdateLikes(data, likes) {
+    var sql = `UPDATE Posts SET likes = '${likes}' WHERE Postid = ${data.PostId};`;
+    Connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Likes Updated")
+    });
+}
+
+
+app.post("/api/Like", (req, res) => {
+    data = req.body
+    if (data.Action === "Like") {
+        var sql = `UPDATE Posts SET LikedBy = 'Raygud' WHERE Postid = ${data.PostId};`;
+        Connection.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Liked");
+            UpdateLikes(data, "1")
+        });
+    }
+    if (data.Action === "Unlike") {
+        var sql = `UPDATE Posts SET LikedBy = '' WHERE Postid = ${data.PostId}`;
+        Connection.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Unliked");
+            UpdateLikes(data, "0")
+        });
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
