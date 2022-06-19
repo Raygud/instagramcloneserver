b6a296b27811e48b9a355d7f6452af9a5febc9e1
Raygud <Runigudmundarson@hotmail.com>
2022-06-19 03:34:21 +0200

    My first commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors')
 const importData = require("./data.json");
 const { send } = require("process");
 const Token = "OnlyRay121294"
-let port = process.env.PORT;
+let port = process.env.PORT || 3001;
 require('dotenv').config()
 app.use(function (req, res, next) {
 
@@ -29,22 +29,32 @@ app.use(function (req, res, next) {
 DataBaseResult = []
 
 
-
-//Login credentials for database
-let Connection = mysql.createConnection({
+const pool = mysql.createPool({
+    connectionLimit: 100, //important
     host: process.env.Host,
     port: process.env.Port,
     user: process.env.User,
     password: process.env.Password,
-    database: process.env.Database
+    database: process.env.Database,
+    debug: false
 });
 
-//Connect too external database
-Connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Connection to Database successful!" + Connection.database);
-});
 
+//Connect too external database -- No longer in use pooling database now.
+// Connection.connect(function (err) {
+//     if (err) throw err;
+//     console.log("Connection to Database successful!" + pool.database);
+// });
+
+// Test if connection was succesfull
+pool.getConnection(function (err, connection) {
+    if (err) {
+        console.log(err)
+        return;
+    }
+    // Any possible tests on `connection` go here...
+    console.log("Connection to Database successful!")
+});
 
 
 app.get("/", (req, res) => {
@@ -52,7 +62,7 @@ app.get("/", (req, res) => {
 })
 
 app.get(`/api`, (req, res) => {
-    Connection.query("SELECT * FROM UsersTest WHERE id='4'", function (err, result, fields) {
+    pool.query("SELECT * FROM UsersTest WHERE id='4'", function (err, result, fields) {
         if (err) throw err;
         exists = result.length
         console.log(result.length);
@@ -67,7 +77,7 @@ app.get(`/api`, (req, res) => {
 })
 
 app.get("/api/characters", (req, res) => {
-    Connection.query("SELECT * FROM characters", function (err, result, fields) {
+    pool.query("SELECT * FROM characters", function (err, result, fields) {
         if (err) throw err;
         console.log(result);
         res.send(result)
@@ -75,11 +85,12 @@ app.get("/api/characters", (req, res) => {
 })
 
 app.get("/api/Posts", (req, res) => {
-    Connection.query("SELECT * FROM Posts", function (err, result, fields) {
+    pool.query("SELECT * FROM Posts", function (err, result, fields) {
         if (err) throw err;
         console.log(result);
         res.send(result)
     });
+
 })
 
 
@@ -93,7 +104,7 @@ app.post("/api/Post", (req, res) => {
     data = req.body
     console.log(data)
     var sql = `INSERT INTO Posts (name, profilePicture, likes, timeStamp, description, comments, image, LikedBy) VALUES ('${data.Name}','${data.ProfilePicture}','${data.Likes}','${data.Time}','${data.Description}','${data.Comments}','${data.image}','${data.Liked}');`;
-    Connection.query(sql, function (err, result) {
+    pool.query(sql, function (err, result) {
         if (err) throw err;
         console.log("1 record inserted");
         res.send(result)
@@ -102,7 +113,7 @@ app.post("/api/Post", (req, res) => {
 
 function UpdateLikes(data, likes) {
     var sql = `UPDATE Posts SET likes = '${likes}' WHERE Postid = ${data.PostId};`;
-    Connection.query(sql, function (err, result) {
+    pool.query(sql, function (err, result) {
         if (err) throw err;
         console.log("Likes Updated")
     });
@@ -113,7 +124,7 @@ app.post("/api/Like", (req, res) => {
     data = req.body
     if (data.Action === "Like") {
         var sql = `UPDATE Posts SET LikedBy = 'Raygud' WHERE Postid = ${data.PostId};`;
-        Connection.query(sql, function (err, result) {
+        pool.query(sql, function (err, result) {
             if (err) throw err;
             console.log("Liked");
             UpdateLikes(data, "1")
@@ -121,7 +132,7 @@ app.post("/api/Like", (req, res) => {
     }
     if (data.Action === "Unlike") {
         var sql = `UPDATE Posts SET LikedBy = '' WHERE Postid = ${data.PostId}`;
-        Connection.query(sql, function (err, result) {
+        pool.query(sql, function (err, result) {
             if (err) throw err;
             console.log("Unliked");
             UpdateLikes(data, "0")
